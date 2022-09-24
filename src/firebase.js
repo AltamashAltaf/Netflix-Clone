a97a1f1a41d7ba384c8a00731e2ea2f46c89de52
AltamashAltaf <71456461+AltamashAltaf@users.noreply.github.com>
2022-09-24 06:01:23 +0530

    netflix-clone to github

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBnD7c2ZO1r-A1tbfn1XbplOwe-Ynx3mjo",
+    authDomain: "netflix-clone-ae156.firebaseapp.com",
+    projectId: "netflix-clone-ae156",
+    storageBucket: "netflix-clone-ae156.appspot.com",
+    messagingSenderId: "296516388138",
+    appId: "1:296516388138:web:24399e540d15e8e9f54a53",
+    measurementId: "G-C8XWJQF26V"
+  };
